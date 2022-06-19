bb14d72e02f36102c8930d845b700f66133cdffa
kenn-san <22313757237@qq.com>
2022-06-19 07:35:45 +0000

    [Args] Adjust arguements for default running

--- run_scripts/inf_msvd_qa.sh
+++ run_scripts/inf_msvd_qa.sh
@@ -10,7 +10,7 @@ CONFIG_PATH='config_release/msvd_qa.json'
 TXT_DB='data/msvd_qa/txt/test.jsonl'
 IMG_DB='data/msvd_qa/videos'
 
-horovodrun -np 8 python src/tasks/run_video_qa.py \
+horovodrun -np 1 python src/tasks/run_video_qa.py \
       --do_inference 1 \
       --inference_split test \
       --inference_model_step $STEP \

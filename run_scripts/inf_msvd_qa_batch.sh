1e7e93c0cecab9c8c44aba5c56223e68649422ec
scysy1 <scysy1@nottingham.edu.cn>
2022-06-21 11:25:36 +0800

    [Git] Update .gitignore

---
+++ run_scripts/inf_msvd_qa_batch.sh
@@ -0,0 +1,27 @@
+cd .. 
+
+export PYTHONPATH="$PYTHONPATH:$PWD"
+echo $PYTHONPATH
+
+
+CONFIG_PATH='config_release/msvd_qa.json'
+
+TXT_DB='data/msvd_qa/txt/test.jsonl'
+IMG_DB='data/msvd_qa/videos'
+
+#Run multiple steps (10 times once)
+#
+
+for (( STEP=1500; STEP<=8100; STEP+=300 ))
+do 
+   echo "Step:$STEP"
+   horovodrun -np 1 python src/tasks/run_video_qa.py \
+      --do_inference 1 \
+      --inference_split test \
+      --inference_model_step $STEP \
+      --inference_txt_db $TXT_DB \
+      --inference_img_db $IMG_DB \
+      --inference_batch_size 32 \
+      --output_dir output/downstreams/msvd_qa/private/20220616134649 \
+      --config $CONFIG_PATH
+done

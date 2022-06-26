f72c3570550ffa4c1900eb4033ecc30c35578152
scysy1 <scysy1@nottingham.edu.cn>
2022-06-27 00:51:53 +0800

    [Model] CLIP encoder separate stream

--- src/modeling/alpro_models.py
+++ src/modeling/alpro_models.py
@@ -649,9 +649,7 @@ class AlproForSequenceClassification(AlproBaseModel):
         self.CLIP_encoder, preprocess = clip.load("ViT-B/32") #no_need for preprocessing the image
 
         self.CLIP_adapter = nn.Sequential(
-            nn.Linear(512 ,
-            config.hidden_size * 2),
-            nn.Linear(config.hidden_size * 2, config.hidden_size)
+            nn.Linear(512, config.hidden_size)
         )
 
         """Freezing CLIP weight as default"""
